e16634c6c88744cf3cfb2d14b77af1bcbceea16d
Sara Hjelt <sarahjelt@Saras-MacBook-Air.local>
2017-10-08 13:25:56 -0400

    creates basic complete UI

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDXOldb8RHiQiy4QwyB0_C42nnA_V609vs",
+    authDomain: "sara-train-scheduler.firebaseapp.com",
+    databaseURL: "https://sara-train-scheduler.firebaseio.com",
+    projectId: "sara-train-scheduler",
+    storageBucket: "sara-train-scheduler.appspot.com",
+    messagingSenderId: "960098055959"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  
