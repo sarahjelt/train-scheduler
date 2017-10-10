fa8d9682c52b4530d175ff9d10468267193905cf
Sara Hjelt <sarahjelt@Saras-MacBook-Air.local>
2017-10-10 19:42:48 -0400

    user can add trains and see time of next arrival and how many mins away

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,55 @@
   };
   firebase.initializeApp(config);
 
+  // Get a reference to the database service
   var database = firebase.database();
 
-  
+$("#submit").on("click", function(event){
+  event.preventDefault();
+
+  var name = $('#name-input').val().trim();
+  console.log(name)
+  var dest = $('#dest-input').val().trim();
+  var firstTrain = $('#first-input').val().trim();
+  var frequency = $('#freq-input').val().trim();
+  var currentTime = moment();
+  var firstTrainConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
+  var diffTime = currentTime.diff(moment(firstTrainConverted), "minutes");
+  var tRemainder = diffTime % frequency;
+  var minutesUntil = frequency - tRemainder;
+  var next = currentTime.add(minutesUntil, "minutes");
+  var nextDisplay = moment(next).format("hh:mm a")
+
+  database.ref().push({ 
+    trainName: name,
+    trainDest: dest,
+    trainFirst: firstTrain,
+    trainFrequency: frequency,
+    nextTime: nextDisplay,
+    minutesAway: minutesUntil
+  })
+
+})
+
+database.ref().on("child_added", function(childSnapshot){
+
+  $('.table').prepend("<tr><td></td><td></td><td></td><td class='next'></td><td class='minutes-away'></td></tr>");  
+
+      var firstRowTds = $("table")
+        .children()
+        .eq(1)
+        .children("tr")
+        .eq(0)
+        .children("td");
+
+      firstRowTds.eq(0).text(childSnapshot.val().trainName);
+
+      firstRowTds.eq(1).text(childSnapshot.val().trainDest);
+
+      firstRowTds.eq(2).text(childSnapshot.val().trainFrequency);
+
+      firstRowTds.eq(3).text(childSnapshot.val().nextTime);
+
+      firstRowTds.eq(4).text(childSnapshot.val().minutesAway);
+
+})
